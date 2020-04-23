7fe69006f3f2f6a91c8714325352b48effc8f016
EjMendez <ej.mendez2904@gmail.com>
2020-04-23 19:44:27 -0300

    Se agrega cliente externo

--- consolaCliente/src/consolaCliente/TestClient.java
+++ consolaCliente/src/consolaCliente/TestClient.java
@@ -1,7 +1,5 @@
 package consolaCliente;
 
-
-import java.util.Hashtable;
 import java.util.List;
 import java.util.Properties;
 
@@ -12,23 +10,35 @@ import javax.naming.NamingException;
 import datatypes.DTRecursos;
 import servletsNegocios.NegocioRecursosRemote;
 
-
 public class TestClient {
-	
 
+	public static void main(String[] args) {
 
-	public static void main(String[] args)  {
-		
+		Properties props = new Properties();
+
+		props.put(Context.INITIAL_CONTEXT_FACTORY, "org.wildfly.naming.client.WildFlyInitialContextFactory");
+
+		props.put(Context.PROVIDER_URL, "remote+http://localhost:8080");
+
+		String jndiName = "ejb:Ejercicio1/Negocios/NegocioRecursos!servletsNegocios.NegocioRecursosRemote";
+
+		try {
+			Context ctx = new InitialContext(props);
+
+			NegocioRecursosRemote conexionRemota = (NegocioRecursosRemote) ctx.lookup(jndiName);
+
+			conexionRemota.addRecursoNegocio("4", 4, 1, "Comestible");
+
+			List<DTRecursos> list = conexionRemota.getRecursos();
 			
-//		NegocioRecursosRemote conexionRemota = (NegocioRecursosRemote)ctx.lookup(jndiName);
-//			conexionRemota.addRecursoNegocio("4", 4, 1, "Comestible");
-//			List<DTRecursos> list = conexionRemota.getRecursos();
-//			for(int i = 0 ; i < list.size(); i++) {
-//				System.out.println(list.get(i).getCodigo());
-//			}
-//	
+			for (int i = 0; i < list.size(); i++) {
+				System.out.println(list.get(i).getCodigo());
+			}
 
+		} catch (NamingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
-	
 
 }
